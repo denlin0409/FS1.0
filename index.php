5bc082b20f8856bee880fc5886f94d9e113e2539
Sofia Garcia <sofgar0614@gmail.com>
2018-11-12 09:28:04 +0100

    created files, created structure and added form.

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title> Fast Security 1.0 </title>
+		<!-- <link href="https://fonts.googleapis.com/css?family=Didact+Gothic|Gothic+A1" rel="stylesheet"> -->
+    <link href="assets/style.css" rel="stylesheet">
+   	<!-- <link rel="shortcut icon" type="image/png" href=" link here "> -->
+  </head>
+  <body>
+    <main>
+
+      <header> <!-- main header -->
+        <img class="logo" src="#" alt="#">
+        <img class="lang" src="#" alt="#">
+      </header>
+
+      <section> <!-- section 1 -->
+        <article>
+
+
+        </article>
+      </section>
+
+      <section> <!-- section 2 -->
+        <article>
+
+
+        </article>
+      </section>
+
+      <section> <!-- section 3 -->
+        <article>
+
+
+        </article>
+      </section>
+
+      <footer> <!-- main footer -->
+
+      </footer>
+
+      <script type="text/javascript" src="assets/script.js"> </script>
+      <script src="http://localhost:35729/livereload.js"> </script>
+    </main>
+  </body>
+</html>
+
+
+<form>
+   <!-- <label for=“name”>Name</label> -->
+   <input id="name" type="text" placeholder="Name" />
+
+   <!-- <label for=“mail”>Email</label> -->
+   <input id="mail" type="email" placeholder="Email" />
+   <button type="submit">Send to email</button>
+ </form>
