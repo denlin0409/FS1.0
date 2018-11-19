8c0a167c74d32fde6a53a525fd4806cf4cf0c254
Sofia Garcia <sofgar0614@gmail.com>
2018-11-19 17:01:25 +0100

    fixed width issues, added dot-menu

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,79 @@
 'use strict';
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Start javascript for dot-menu and header-functions
+  const section1 = document.querySelector('.section-1');
+  const section2 = document.querySelector('.section-2');
+  const section3 = document.querySelector('.section-3');
+  const section4 = document.querySelector('.section-4');
+  const mainHeader = document.querySelector('.main-header');
+
+  const headerHeight = mainHeader.getBoundingClientRect().height;
+  const section2Offset = section2.getBoundingClientRect().y;
+  const section3Offset = section3.getBoundingClientRect().y;
+  const section4Offset = section4.getBoundingClientRect().y;
+
+  const dottMenu = document.querySelector('.dott-menu');
+  const infoSpanDott = dottMenu.querySelector('.info');
+  const introSpanDott = dottMenu.querySelector('.intro');
+  const contactSpanDott = dottMenu.querySelector('.contact');
+  const moreSpanDott = dottMenu.querySelector('.more');
+
+  let myClassList = 0;
+  const funcActiveDott = (activeClass) => {
+
+    myClassList.replace("fa-circle-o", "fa-circle");
+    let dottsList = dottMenu.querySelectorAll('span');
+    dottsList = Array.from(dottsList);
+
+    for (let i = 0; i < dottsList.length; i++) {
+
+      if (dottsList[i].children['0'].classList.contains(activeClass)){
+        continue;
+      }
+
+      dottsList[i].children['0'].classList.replace("fa-circle", "fa-circle-o");
+    }
+  }
+   window.addEventListener('scroll',()=>{
+    const offset = window.pageYOffset;
+    if(offset >= headerHeight){
+      if(! mainHeader.classList.contains('header-wh')){
+        mainHeader.classList.add('header-wh');
+      }
+    }
+
+    if(offset >= 0 && offset <= headerHeight - 10){
+      mainHeader.classList.remove('header-wh');
+      myClassList = introSpanDott.children['0'].classList;
+      funcActiveDott(myClassList['0']);
+    }
+
+    if(offset >= section2Offset - headerHeight && offset <= section2Offset + headerHeight){
+      myClassList = infoSpanDott.children['0'].classList;
+      funcActiveDott(myClassList['0']);
+    };
+
+    if(offset >= section3Offset - headerHeight && offset <= section3Offset + headerHeight){
+      myClassList = contactSpanDott.children['0'].classList;
+      funcActiveDott(myClassList['0']);
+    };
+
+    if(offset >= section4Offset - headerHeight && offset <= section4Offset + headerHeight){
+      myClassList = moreSpanDott.children['0'].classList;
+      funcActiveDott(myClassList['0']);
+    };
+  });
+   introSpanDott.addEventListener('click',()=>{
+    window.scrollTo(0, 0);
+  });
+   infoSpanDott.addEventListener('click',()=>{
+    window.scrollTo(0, section2Offset);
+  });
+   contactSpanDott.addEventListener('click',()=>{
+    window.scrollTo(0, section3Offset);
+  });
+   moreSpanDott.addEventListener('click',()=>{
+    window.scrollTo(0, section4Offset);
+  });
+});
--- index.php
+++ index.php
@@ -11,21 +11,32 @@ require(__DIR__.'/assets/languages.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title> Fast Security 1.0 </title>
 		<link href="https://fonts.googleapis.com/css?family=Archivo+Black" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link href="assets/style.css" rel="stylesheet">
    	<!-- <link rel="shortcut icon" type="image/png" href=" link here "> -->
   </head>
   <body>
     <main>
 
-      <section> <!-- section 1 -->
-        <header> <!-- main header -->
-          <div class="header-bg">
-            <img class="logo" src="#" alt="#">
-            <img class="lang" src="#" alt="#">
-            <a href="?lang=eng"> engelska </a>
-            <a href="?lang=swe"> svenska </a>
-          </div>
-        </header>
+      <!-- Start Floated dot-menu -->
+      <div class="dott-menu">
+        <span class="intro"><i class="intro fa fa-circle" aria-hidden="true"></i>Intro</span>
+        <span class="info"><i class="info fa fa-circle-o" aria-hidden="true"></i>Info</span>
+        <span class="contact"><i class="contact fa fa-circle-o" aria-hidden="true"></i>Contact</span>
+        <span class="more"><i class="more fa fa-circle-o" aria-hidden="true"></i>More</span>
+        <button type="button" name="button">Form</button>
+      </div>
+      <!-- End Floated dot-menu -->
+
+      <header class="main-header"> <!-- main header -->
+        <div class="header-bg">
+          <img class="logo" src="assets/images/placeholder.jpg" alt="#">
+          <a href="?lang=eng"> engelska </a>
+          <a href="?lang=swe"> svenska </a>
+        </div>
+      </header>
+
+      <section class="section-1"> <!-- section 1 -->
         <div class="first-page-img">
           <article class="title">
             <h1> <?= $languages[$lang]['titleS1']; ?> </h1>
@@ -33,7 +44,7 @@ require(__DIR__.'/assets/languages.php');
         </div>
       </section>
 
-      <section class="s-padding"> <!-- section 2 -->
+      <section class="s-padding section-2"> <!-- section 2 -->
         <div class="box-1">
           <img src="assets/images/placeholder.jpg" alt="#" width="100%" height="100%">
         </div>
@@ -53,7 +64,7 @@ require(__DIR__.'/assets/languages.php');
         </div>
       </section>
 
-      <section> <!-- section 3 -->
+      <section class="section-3"> <!-- section 3 -->
         <div class="first-page-img">
           <article class="title">
             <h1> <?= $languages[$lang]['titleF2']; ?> </h1>
@@ -63,7 +74,7 @@ require(__DIR__.'/assets/languages.php');
         </div>
       </section>
 
-      <section class="s-padding"> <!-- section 4 -->
+      <section class="s-padding section-4"> <!-- section 4 -->
         <div class="picture-container">
           <div class="picture big-picture">
             <img src="assets/images/placeholder.jpg" alt="#" width="100%" height="100%">
@@ -91,7 +102,7 @@ require(__DIR__.'/assets/languages.php');
         </article>
       </section>
 
-      <section> <!-- section 5 -->
+      <section class="section-5"> <!-- section 5 -->
         <article>
 
           <h1 style="height: 1070px;"> TEMPORARY TEXT </h1> <!-- REMOVE LATER -->
@@ -99,12 +110,12 @@ require(__DIR__.'/assets/languages.php');
         </article>
       </section>
 
-      <footer> <!-- main footer -->
+      <footer class="main-footer"> <!-- main footer -->
         <h1 style="height: 500px;"> TEMPORARY TEXT </h1> <!-- REMOVE LATER -->
       </footer>
 
       <script type="text/javascript" src="assets/script.js"> </script>
-      <script src="http://localhost:35729/livereload.js"> </script>
+      <!-- <script src="http://localhost:35729/livereload.js"> </script> -->
     </main>
   </body>
 </html>
