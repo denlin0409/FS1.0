9e4b9750197cd044cd4a4a8f2af94d2fc0770ce3
Sofia Garcia <sofgar0614@gmail.com>
2018-11-24 01:47:58 +0100

    changed dot-menu, added split-view

--- assets/script.js
+++ assets/script.js
@@ -1,79 +1,66 @@
 'use strict';
+const spanDotBtns = document.querySelectorAll('.dot-btn');
+const mainHeader = document.querySelector('.main-header');
+const allSections = document.querySelectorAll('.section-component');
+const mainHeaderHeight = mainHeader.getBoundingClientRect().height;
 
-document.addEventListener("DOMContentLoaded", () => {
-  // Start javascript for dot-menu and header-functions
-  const section1 = document.querySelector('.section-1');
-  const section2 = document.querySelector('.section-2');
-  const section3 = document.querySelector('.section-3');
-  const section4 = document.querySelector('.section-4');
-  const mainHeader = document.querySelector('.main-header');
+const options = {
+  threshold: 0.5
+}
 
-  const headerHeight = mainHeader.getBoundingClientRect().height;
-  const section2Offset = section2.getBoundingClientRect().y;
-  const section3Offset = section3.getBoundingClientRect().y;
-  const section4Offset = section4.getBoundingClientRect().y;
+for (let i = 0; i < spanDotBtns.length; i++) {
+    spanDotBtns[i].addEventListener('click',(event) => {
+        let id=event.target.dataset.section;
+        document.getElementById(id).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    });
+};
 
-  const dottMenu = document.querySelector('.dott-menu');
-  const infoSpanDott = dottMenu.querySelector('.info');
-  const introSpanDott = dottMenu.querySelector('.intro');
-  const contactSpanDott = dottMenu.querySelector('.contact');
-  const moreSpanDott = dottMenu.querySelector('.more');
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(function(entry){
+        let dataSet = entry.target.id;
+        let dotBtn = document.querySelector(`[data-section=${CSS.escape(dataSet)}]`);
+        if(entry.intersectionRatio > 0){
+            dotBtn.children['0'].classList.toggle("dot-f");
+        }
+    });
 
-  let myClassList = 0;
-  const funcActiveDott = (activeClass) => {
+}, options);
 
-    myClassList.replace("fa-circle-o", "fa-circle");
-    let dottsList = dottMenu.querySelectorAll('span');
-    dottsList = Array.from(dottsList);
+allSections.forEach((section) => {
+    observer.observe(section);
+});
 
-    for (let i = 0; i < dottsList.length; i++) {
+window.addEventListener('scroll',() => {
+    let offset = window.pageYOffset;
 
-      if (dottsList[i].children['0'].classList.contains(activeClass)){
-        continue;
-      }
-
-      dottsList[i].children['0'].classList.replace("fa-circle", "fa-circle-o");
-    }
-  }
-   window.addEventListener('scroll',()=>{
-    const offset = window.pageYOffset;
-    if(offset >= headerHeight){
-      if(! mainHeader.classList.contains('header-wh')){
-        mainHeader.classList.add('header-wh');
-      }
-    }
-
-    if(offset >= 0 && offset <= headerHeight - 10){
+    if(offset >= 0 && offset <= mainHeaderHeight - 20){
       mainHeader.classList.remove('header-wh');
-      myClassList = introSpanDott.children['0'].classList;
-      funcActiveDott(myClassList['0']);
-    }
-
-    if(offset >= section2Offset - headerHeight && offset <= section2Offset + headerHeight){
-      myClassList = infoSpanDott.children['0'].classList;
-      funcActiveDott(myClassList['0']);
     };
 
-    if(offset >= section3Offset - headerHeight && offset <= section3Offset + headerHeight){
-      myClassList = contactSpanDott.children['0'].classList;
-      funcActiveDott(myClassList['0']);
+    if (offset >= mainHeaderHeight){
+        mainHeader.classList.add("header-wh");
     };
+});
 
-    if(offset >= section4Offset - headerHeight && offset <= section4Offset + headerHeight){
-      myClassList = moreSpanDott.children['0'].classList;
-      funcActiveDott(myClassList['0']);
-    };
-  });
-   introSpanDott.addEventListener('click',()=>{
-    window.scrollTo(0, 0);
-  });
-   infoSpanDott.addEventListener('click',()=>{
-    window.scrollTo(0, section2Offset);
-  });
-   contactSpanDott.addEventListener('click',()=>{
-    window.scrollTo(0, section3Offset);
-  });
-   moreSpanDott.addEventListener('click',()=>{
-    window.scrollTo(0, section4Offset);
-  });
+
+
+const BORDER_SIZE = 4;
+const panel = document.getElementById("right_panel");
+
+let m_pos;
+function resize(e){
+  const dx = m_pos - e.x;
+  m_pos = e.x;
+  panel.style.width = (parseInt(getComputedStyle(panel, '').width) + dx) + "px";
+}
+
+panel.addEventListener("mousedown", function(e){
+  if (e.offsetX < BORDER_SIZE) {
+    m_pos = e.x;
+    document.addEventListener("mousemove", resize);
+  }
+});
+
+document.addEventListener("mouseup", function(){
+    document.removeEventListener("mousemove", resize);
 });
--- index.php
+++ index.php
@@ -9,24 +9,25 @@ require(__DIR__.'/assets/languages.php');
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> Fast Security 1.0 </title>
 		<link href="https://fonts.googleapis.com/css?family=Archivo+Black" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link href="assets/style.css" rel="stylesheet">
+
    	<!-- <link rel="shortcut icon" type="image/png" href=" link here "> -->
   </head>
   <body>
     <main>
 
-      <!-- Start Floated dot-menu -->
-      <div class="dott-menu">
-        <span class="intro"><i class="intro fa fa-circle" aria-hidden="true"></i>Intro</span>
-        <span class="info"><i class="info fa fa-circle-o" aria-hidden="true"></i>Info</span>
-        <span class="contact"><i class="contact fa fa-circle-o" aria-hidden="true"></i>Contact</span>
-        <span class="more"><i class="more fa fa-circle-o" aria-hidden="true"></i>More</span>
-        <button type="button" name="button">Form</button>
+      <!-- Start dot-menu -->
+      <div class="dot-menu">
+        <span class="dot-btn" data-section="S01"><div class="dot-o"></div></i>Intro</span>
+        <span class="dot-btn" data-section="S02"><div class="dot-o"></div></i>Info</span>
+        <span class="dot-btn" data-section="S03"><div class="dot-o"></div></i>Contact</span>
+        <span class="dot-btn" data-section="S04"><div class="dot-o"></div></i>More</span>
       </div>
-      <!-- End Floated dot-menu -->
+      <!-- End dot-menu -->
 
       <header class="main-header"> <!-- main header -->
         <div class="header-bg">
@@ -36,7 +37,7 @@ require(__DIR__.'/assets/languages.php');
         </div>
       </header>
 
-      <section class="section-1"> <!-- section 1 -->
+      <section id="S01" class="section-component"> <!-- section 1 -->
         <div class="first-page-img">
           <article class="title">
             <h1> <?= $languages[$lang]['titleS1']; ?> </h1>
@@ -44,27 +45,29 @@ require(__DIR__.'/assets/languages.php');
         </div>
       </section>
 
-      <section class="s-padding section-2"> <!-- section 2 -->
-        <div class="box-1">
-          <img src="assets/images/placeholder.jpg" alt="#" width="100%" height="100%">
-        </div>
-        <article class="container">
-          <div class="text-container">
-            <h2 class="text-padding text-color"> <?= $languages[$lang]['titleF']; ?> </h2>
-            <p class="text-padding text-color"> <?= $languages[$lang]['titleS2']; ?> </p>
-            <p class="text-color"> <?= $languages[$lang]['textS2']; ?> </p>
+      <section id="S02" class="section-component"> <!-- section 2 -->
+        <div class="wrapper">
+          <div class="box-1">
+            <img src="assets/images/placeholder.jpg" alt="#" width="100%" height="100%">
           </div>
-        </article>
-        <div>
-          <form class="form">
-           <!-- <label for=“mail”>Email</label> -->
-           <input id="mail" type="email" placeholder="Email" />
-           <button type="submit">Send to email</button>
-          </form>
+          <article class="container">
+            <div class="text-container">
+              <h2 class="text-padding text-color"> <?= $languages[$lang]['titleF']; ?> </h2>
+              <p class="text-padding text-color"> <?= $languages[$lang]['titleS2']; ?> </p>
+              <p class="text-color"> <?= $languages[$lang]['textS2']; ?> </p>
+            </div>
+          </article>
+          <div>
+            <form class="form">
+             <!-- <label for=“mail”>Email</label> -->
+             <input id="mail" type="email" placeholder="Email" />
+             <button type="submit">Send to email</button>
+            </form>
+          </div>
         </div>
       </section>
 
-      <section class="section-3"> <!-- section 3 -->
+      <section id="S03" class="section-component"> <!-- section 3 -->
         <div class="first-page-img">
           <article class="title">
             <h1> <?= $languages[$lang]['titleF2']; ?> </h1>
@@ -74,39 +77,43 @@ require(__DIR__.'/assets/languages.php');
         </div>
       </section>
 
-      <section class="s-padding section-4"> <!-- section 4 -->
-        <div class="picture-container">
-          <div class="picture big-picture">
-            <img src="assets/images/placeholder.jpg" alt="#" width="100%" height="100%">
+      <section id="S04" class="section-component"> <!-- section 4 -->
+        <div class="wrapper">
+          <div class="picture-container">
+            <div class="picture big-picture">
+              <img src="assets/images/placeholder.jpg" alt="#" width="100%" height="100%">
+            </div>
+            <div class="picture">
+              <img src="assets/images/placeholder.jpg" alt="#" width="100%" height="100%">
+            </div>
+            <div class="picture">
+              <img src="assets/images/placeholder.jpg" alt="#" width="100%" height="100%">
+            </div>
           </div>
-          <div class="picture">
-            <img src="assets/images/placeholder.jpg" alt="#" width="100%" height="100%">
-          </div>
-          <div class="picture">
-            <img src="assets/images/placeholder.jpg" alt="#" width="100%" height="100%">
-          </div>
-        </div>
-        <article class="features-container">
-          <div class="text-features">
-            <h3> <?= $languages[$lang]['titleS2']; ?> </h3>
-            <p> <?= $languages[$lang]['textS2']; ?> </p>
-          </div>
-          <div class="text-features">
-            <h3> <?= $languages[$lang]['titleS2']; ?> </h3>
-            <p> <?= $languages[$lang]['textS2']; ?> </p>
-          </div>
-          <div class="text-features">
-            <h3> <?= $languages[$lang]['titleS2']; ?> </h3>
-            <p> <?= $languages[$lang]['textS2']; ?> </p>
+          <article class="features-container">
+            <div class="text-features">
+              <h3> <?= $languages[$lang]['titleS2']; ?> </h3>
+              <p> <?= $languages[$lang]['textS2']; ?> </p>
+            </div>
+            <div class="text-features">
+              <h3> <?= $languages[$lang]['titleS2']; ?> </h3>
+              <p> <?= $languages[$lang]['textS2']; ?> </p>
+            </div>
+            <div class="text-features">
+              <h3> <?= $languages[$lang]['titleS2']; ?> </h3>
+              <p> <?= $languages[$lang]['textS2']; ?> </p>
+            </div>
           </div>
         </article>
       </section>
 
-      <section class="section-5"> <!-- section 5 -->
+      <section> <!-- section 5 -->
         <article>
-
-          <h1 style="height: 1070px;"> TEMPORARY TEXT </h1> <!-- REMOVE LATER -->
-
+          <div class="left-panel">
+            <div class="right_panel" id="right_panel">
+              <i class="fa fa-arrows-h" aria-hidden="true"></i>
+            </div>
+          </div>
         </article>
       </section>
 
@@ -115,7 +122,7 @@ require(__DIR__.'/assets/languages.php');
       </footer>
 
       <script type="text/javascript" src="assets/script.js"> </script>
-      <!-- <script src="http://localhost:35729/livereload.js"> </script> -->
+      <script src="http://localhost:35729/livereload.js"> </script>
     </main>
   </body>
 </html>
